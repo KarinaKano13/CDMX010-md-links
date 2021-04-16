1db7c286527892b6769832ae66d860ff5fd99134
KarinaKano13 <75265602+KarinaKano13@users.noreply.github.com>
2021-04-15 22:43:22 -0500

    al the funcitons update

--- filestwo/two.md
+++ filestwo/two.md
@@ -1,2 +1,3 @@
 * [Leer un archivo](https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback)
 * [Leer un directorio](https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback)
+* [Leer un directorio](https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback)
--- mdLinks.js
+++ mdLinks.js
@@ -5,17 +5,10 @@ const fetch = require('node-fetch');
 const process = require('process');
 const absoluteUrlExpression = /\(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}\gi/gm
 
-const options = {
-    one: process.argv[2],
-    two: process.argv[3]
-};
-
 
 /* --------- CONTORLADOR GENERAL ---------- */
 const controller = (file, options) => {
-    let links = []
-    const linksResult = fs.readFileSync(file, 'utf-8').match(absoluteUrlExpression)
-    
+    const linksResult = fs.readFileSync(file, 'utf-8').match(absoluteUrlExpression) 
         if (options.two === undefined) {
             option = options.one
         } else if(options.two !== undefined){
@@ -31,9 +24,7 @@ const controller = (file, options) => {
                 showingLinks(linksResult, file);
             }
 };
-
 const statsAndValidate = (link, file) => {
-
     let Links = link.map((allLinks) => {
         const linksAbsolute = allLinks.slice(1,-1);
         
@@ -42,14 +33,11 @@ const statsAndValidate = (link, file) => {
                return res
             }).catch((err) => console.log(err)) 
      })
-    // console.log(brokeLinks);
     return Promise.all(Links)
          .then((res) =>  statsValid(res, link, file))
          .catch((err) => console.log(err))
 };
-
 const validatingLinks = (file, link) => {
-
     link.forEach((allLinks) => {
         const linksAbsolute = allLinks.slice(1,-1);
         fetch(linksAbsolute,['GET'])
@@ -58,13 +46,11 @@ const validatingLinks = (file, link) => {
             }).catch((err) => console.log(err)) 
     })               
 };
-
 const statsValid = (res, link, file) => {
     let totalLinks=link.length; 
     const uniqueLinks = [...new Set(link)].length; 
     const brokeLinks = []
     const arrayRes = res.forEach(res => {
-        // console.log(res.status);
         if (res.status !== 200) {
             brokeLinks.push(res)
             return brokeLinks
@@ -78,7 +64,6 @@ const statsValid = (res, link, file) => {
     }
         showingStatsAndValidate(linksStats);
 };
-
 const stats = (link, file) => {
     let totalLinks=link.length; 
     const uniqueLinks = [...new Set(link)].length; 
@@ -93,44 +78,40 @@ const stats = (link, file) => {
 /* --------- < FUNCIÓNES QUE RENDERIZAN RESULTADOS EN LA CONSOLA > ---------- */
 const showingStatsAndValidate = (linksStats) => {
     setTimeout(() => {
-        console.log(("    "), chalk.whiteBright.bold("FILE NAME: "),chalk.greenBright.bold(linksStats.NAME));
-        console.log(("    "), chalk.yellowBright.bold("TOTAL-LINKS: "),chalk.blueBright.bold(linksStats.TOTALS));
-        console.log(("    "), chalk.yellowBright.bold("UNIQUE-LINKS:"),chalk.blueBright.bold(linksStats.UNIQUE));
-        console.log(("    "), chalk.yellowBright.bold("BROKEN-LINKS:"),chalk.redBright.bold(linksStats.BROKE));
+        console.log(("      "), chalk.whiteBright.bold("FILE NAME: "),chalk.greenBright.bold(linksStats.NAME));
+        console.log((" "))
+        console.log(("      "), chalk.yellowBright.bold("TOTAL-LINKS: "),chalk.blueBright.bold(linksStats.TOTALS));
+        console.log(("      "), chalk.yellowBright.bold("UNIQUE-LINKS:"),chalk.blueBright.bold(linksStats.UNIQUE));
+        console.log(("      "), chalk.yellowBright.bold("BROKEN-LINKS:"),chalk.redBright.bold(linksStats.BROKE));
         console.log((" "));
     },1500)
-}
-
+};
 const showingStats = (stats) =>{
     setTimeout(() => {
-        console.log(("  "), chalk.whiteBright.bold("FILE: "),chalk.greenBright.bold(stats.NAME));
+        console.log(("      "), chalk.whiteBright.bold("FILE: "),chalk.greenBright.bold(stats.NAME));
         console.log((" "))
-        console.log(("  "), chalk.yellowBright.bold("TOTAL-LINKS:"),chalk.blueBright.bold(stats.TOTALS))
-        console.log(("  "), chalk.yellowBright.bold("UNIQUE-LINKS:"),chalk.blueBright.bold(stats.UNIQUE))
+        console.log(("      "), chalk.yellowBright.bold("TOTAL-LINKS:"),chalk.blueBright.bold(stats.TOTALS))
+        console.log(("      "), chalk.yellowBright.bold("UNIQUE-LINKS:"),chalk.blueBright.bold(stats.UNIQUE))
         console.log((" "))
     }, 1500)
 };
-
 const showingValidLinks = (path, res, link) =>{
     if(res.status === 404){
-        console.log(("  "), chalk.gray.bold(path), chalk.blueBright.bold (link.slice(0,25)),'...' ,chalk.redBright.bold("FAIL"), chalk.yellowBright.bold(res.status))
+        console.log(("      "), chalk.gray.bold(path), chalk.blueBright.bold (link.slice(0,25)) ,chalk.redBright.bold("FAIL"), chalk.yellowBright.bold(res.status))
     } else if (res.status !== 400){
-        console.log(("  "), chalk.gray.bold(path), chalk.blueBright.bold (link.slice(0,25)),'...',chalk.greenBright.bold(res.statusText), chalk.yellowBright.bold(res.status))
+        console.log(("      "), chalk.gray.bold(path), chalk.blueBright.bold (link.slice(0,25)),chalk.greenBright.bold(res.statusText), chalk.yellowBright.bold(res.status))
     } 
 };
-
 const showingLinks =  (links, file) => {
     links.forEach(link => {
        link.slice(1,-1)  
-       console.log(("  "), chalk.gray.bold(file),chalk.green.bold(link.slice(1,40)))
+       console.log(("      "), chalk.gray.bold(file.slice(0,25)),chalk.green.bold(link.slice(1,40)))
     })
     console.log(("  "));
 };
 
 /* --------- TODA ESTA PARTE REALIZA LA VALIDACIÓN, DE SI ES UNA CARPETA O UN ARCHIVO .MD ---------- */ 
-
-// THIS FUNCTION RETURNS THE COMPLETE FILEPATHS ON A DIRECTORY
-const getPathFinal = (files,directory, options) => {
+const getPathFinal = (files, directory, options) => {
     let filePathFinal = [];
         files.forEach(element => {
             const newPath = path.resolve(directory,element)
@@ -142,7 +123,6 @@ const getPathFinal = (files,directory, options) => {
         });
         return filePathFinal
 };
-
 const getFilesFromDirectory = (directory, options) => {
     const files = fs.readdirSync(directory,'utf8')
         let pathFinal = getPathFinal(files, directory, options)
@@ -151,7 +131,6 @@ const getFilesFromDirectory = (directory, options) => {
             mdLinksFunction(mdFiles, options)
         })
 };
-
 const mdLinksFunction = (file, options) => {
     const files = fs.statSync(file)  
     if (files.isDirectory()) {
@@ -159,26 +138,20 @@ const mdLinksFunction = (file, options) => {
     }else if (files.isFile()) {
         const extension = path.extname(file)
             if (extension === ".md") {
-                //console.log(file);
                 controller(file, options)
             }};
 }; 
 
+const mdLinks = () => {
+    console.log(("         "));
+    console.log(("    "), chalk.bgWhite.black("                              ANALYZING                              "));
+    console.log(("         "));
+    const options = {
+        one: process.argv[3],
+        two: process.argv[4],
+    };  
+    const file = process.argv[2];
+    mdLinksFunction(file, options);
+}
 
-mdLinksFunction("./fileOne.md", options);
-
-// const mdLinks = () => {
-//     const options = {
-//         one: process.argv[3],
-//         two: process.argv[4],
-//     };  
-//     const file = process.argv[2];
-
-//     mdLinksFunction(file, options);
-// }
-
-// mdLinks()
-
-// mdLinks("./README.md", options)
-// mdLinks("filestwo", options)
-
+mdLinks()
